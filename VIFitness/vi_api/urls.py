8e33b325dd74c8bba91745c3f7c5692b6f4d593e
Oscarqjh <aerialguard@gmail.com>
2023-09-11 15:07:01 +0800

    delete

--- VIFitness/vi_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("api", views.UserDataApiView.as_view()),
-    path("api/<str:userdata_id>/", views.UserDataDetailApiView.as_view()),
-]
