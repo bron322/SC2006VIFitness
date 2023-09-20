e37aa913d39c9725f189b9d5fd55e4974d1459bc
ngyh6726 <ngyh6726@gmail.com>
2023-09-20 23:21:36 +0800

    Ng done with loginpage

--- VIFitnessFrontend/src/components/headerlogin.jsx
+++ VIFitnessFrontend/src/components/headerlogin.jsx
@@ -1,12 +1,18 @@
-import React, { Component, useState, useEffect } from 'react'
-import './styles/header.css'
-import VILOGO from './styles/photos/VILOGO.jpg'
+import React, { Component, useState, useEffect, useContext} from "react";
+import "./styles/header.css";
+import VILOGO from "./styles/photos/VILOGO.jpg";
 import Button from "../components/button";
-
-
+import { IconButton } from "@mui/material";
+import { ColorModeContext, tokens } from "../routes/theme";
+import { useTheme } from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
 function Header() {
   const [isSticky, setIsSticky] = useState(false);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
   // Function to handle the scroll event
   function handleScroll() {
@@ -19,30 +25,47 @@ function Header() {
 
   // Add a scroll event listener to determine when the header becomes sticky
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Clean up the event listener when the component unmounts
     return function cleanup() {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-
   return (
-      <div className={`sticky-header ${isSticky ? 'animate' : ''}`}> 
-          <div className="logo-container">
-            <a href="/" className='white-link'>
-              <img className='img'
-              src = {VILOGO}
-              alt = "LOGO" 
-              style={{ width: '30%', height: '30%', display: 'block', overflow: 'hidden'}}></img>
-              </a>
-          </div>
-          <div className="login-register-container">
-            <Button name = "logout"/>
-          </div>
+    <>
+      <div className={`sticky-header ${isSticky ? "animate" : ""}`} style={{ position: 'relative', zIndex: 20 }}>
+        <div className="logo-container">
+          <a href="/" className="white-link">
+            <img
+              className="img"
+              src={VILOGO}
+              alt="LOGO"
+              style={{
+                width: "30%",
+                height: "30%",
+                display: "block",
+                overflow: "hidden",
+                filter: "invert(100%)"
+              }}
+            ></img>
+          </a>
+        </div>
+        <div className="login-register-container">
+          <Button name="login" />
+          <Button theme="blue" name="register" />
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+        </div>
       </div>
-    );
+    </>
+  );
 }
 
 export default Header;
--- VIFitnessFrontend/src/routes/LoginPage.jsx
+++ VIFitnessFrontend/src/routes/LoginPage.jsx
@@ -1,11 +1,139 @@
+import React, { useState } from "react";
+import './styles/loginpage.css'
+import BG from './styles/photos/loginbackground.jpg'
+import therock from './styles/photos/therock.png'
+import theking from './styles/photos/theking.png'
+import TextField from '@mui/material/TextField';
+import Header from "../components/headerlogin";
+
 export default function LoginPage() {
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container font-semibold">
+          {/* <label className="text-gray-500">USERNAME</label>
+          <input className="bg-logincolor" type="text" name="uname" required /> */}
+          <TextField id="standard-basic" label="USERNAME" variant="standard" margin="dense"/>
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container font-semibold">
+          {/* <label className="text-gray-500">PASSWORD</label>
+          <input className="bg-logincolor" type="password" name="pass" required /> */}
+          <TextField id="standard-basic" label="PASSWORD" variant="standard" margin="dense"/>
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container pt-5">
+          <input type="submit" value="LOG IN" style={{ width: '90%'}}/>
+        </div>
+      </form>
+    </div>
+  );
+
   return (
     <>
-      <div className="login-page-wrapper">
-        <div className="text-xl">
-          Hello world
+      <Header/>
+      <div className="h-[770px] flex items-center justify-center bg-black">
+        <div className="h-4/5 w-4/5 bg-logincolor flex items-center justify-center"> 
+          <div className="flex-1 w-full h-full p-4 flex-col items-center justify-center"> 
+
+            <div className="pt-32 pl-32 pr-32 h-1/3 w-full flex flex-col items-left">
+              <div className="text-7xl font-sans font-bold mb-4 text-left uppercase">Log In</div>
+              {isSubmitted ? (
+                <div>User is successfully logged in</div>
+              ) : (
+                renderForm
+              )}
+              </div>
+              <div className="">
+
+              </div>
+            </div>
+
+            <div className="flex-1 w-full h-full"> 
+              <img
+                className="img w-full h-full"
+                src={BG}
+                alt="background"
+                style={{
+                  display: "block",
+                  overflow: "hidden",
+                  objectFit: "cover" 
+                }}
+              ></img>
+            </div>
+
+            <div>
+              
+            </div>
         </div>
       </div>
+
+      <div className="absolute w-1/2 h-[770px] top-20 left-1/3 right-0 bottom-0 pl-24 flex justify-center items-center pointer-events-none"> 
+              <img
+                className="img h-[750px] w-[550px]"
+                src={therock}
+                alt="the_king"
+                style={{
+                  display: "absolute",
+                  overflow: "hidden",
+                  objectFit: "cover",
+                  zIndex: "3", 
+                  pointerEvents: "none",
+                }}
+              ></img>
+            </div>          
+
     </>
   );
 }
