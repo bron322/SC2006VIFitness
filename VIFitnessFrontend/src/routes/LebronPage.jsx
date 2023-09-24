0f25b26d4bfbe352737996390a91a956155528de
bron322 <lebron13145@gmail.com>
2023-09-24 19:14:02 +0800

    edited the top and sidebar

--- VIFitnessFrontend/src/routes/LebronPage.jsx
+++ VIFitnessFrontend/src/routes/LebronPage.jsx
@@ -6,11 +6,11 @@ import { Modal } from "../components/Modal";
 import PieChartLebron from "../components/piechart";
 import Semircirclebar from "../components/semircirclebar"
 
-import Topbar from "./topbar";
-import Sidebar from "./sidebars";
+import Topbar from "../components/topbar";
+import { MyProSidebarProvider } from "../components/sidebarContext"
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import './styles/profilePage.css' 
+import './styles/profilePage.css'
 
 //This is Lebron test micro page
 
@@ -49,12 +49,12 @@ export default function LebronPage() {
     rowToEdit === null
       ? setRows([...rows, newRow])
       : setRows(
-          rows.map((currRow, idx) => {
-            if (idx !== rowToEdit) return currRow;
+        rows.map((currRow, idx) => {
+          if (idx !== rowToEdit) return currRow;
 
-            return newRow;
-          })
-        );
+          return newRow;
+        })
+      );
   };
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -63,74 +63,75 @@ export default function LebronPage() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-    <div className="app w-5/6">
-    <Sidebar isSidebar={isSidebar} />
-    <main className="content">
-    <Topbar setIsSidebar={setIsSidebar} />
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
 
 
-    <div className="App">
-      <div className="piechart-container flex w-5/6 h-1/3 justify-center items-center pt-10">
-        <Semircirclebar/>
-      </div>
-      <div className="piechart-container flex w-5/6 h-1/3 justify-center items-center">
-        <PieChartLebron/>
-      </div>
+              <div className="App">
+                <div className="piechart-container flex w-5/6 h-1/3 justify-center items-center pt-10">
+                  <Semircirclebar />
+                </div>
+                <div className="piechart-container flex w-5/6 h-1/3 justify-center items-center">
+                  <PieChartLebron />
+                </div>
 
 
-      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
-        <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
-          Add
-        </button> 
-      {modalOpen && (
-        <Modal
-          closeModal={() => {
-            setModalOpen(false);
-            setRowToEdit(null);
-          }}
-          onSubmit={handleSubmit}
-          defaultValue={rowToEdit !== null && rows[rowToEdit]}
-        />
-      )}
-      <div className="relative flex h-[50px] w-full"/>
+                <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+                <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
+                  Add
+                </button>
+                {modalOpen && (
+                  <Modal
+                    closeModal={() => {
+                      setModalOpen(false);
+                      setRowToEdit(null);
+                    }}
+                    onSubmit={handleSubmit}
+                    defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                  />
+                )}
+                <div className="relative flex h-[50px] w-full" />
 
 
 
-      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
-        <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
-          Add
-        </button> 
-      {modalOpen && (
-        <Modal
-          closeModal={() => {
-            setModalOpen(false);
-            setRowToEdit(null);
-          }}
-          onSubmit={handleSubmit}
-          defaultValue={rowToEdit !== null && rows[rowToEdit]}
-        />
-      )}
-      <div className="relative flex h-[50px] w-full"/>
+                <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+                <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
+                  Add
+                </button>
+                {modalOpen && (
+                  <Modal
+                    closeModal={() => {
+                      setModalOpen(false);
+                      setRowToEdit(null);
+                    }}
+                    onSubmit={handleSubmit}
+                    defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                  />
+                )}
+                <div className="relative flex h-[50px] w-full" />
 
-      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
-        <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
-          Add
-        </button> 
-      {modalOpen && (
-        <Modal
-          closeModal={() => {
-            setModalOpen(false);
-            setRowToEdit(null);
-          }}
-          onSubmit={handleSubmit}
-          defaultValue={rowToEdit !== null && rows[rowToEdit]}
-        />
-      )}
-      <div className="relative flex h-[50px] w-full"/>
-    </div>
-    </main>
-    </div>
-    </ThemeProvider>
+                <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+                <button onClick={() => setModalOpen(true)} className="btn flex justify-self-center">
+                  Add
+                </button>
+                {modalOpen && (
+                  <Modal
+                    closeModal={() => {
+                      setModalOpen(false);
+                      setRowToEdit(null);
+                    }}
+                    onSubmit={handleSubmit}
+                    defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                  />
+                )}
+                <div className="relative flex h-[50px] w-full" />
+              </div>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
