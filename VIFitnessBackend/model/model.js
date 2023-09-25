fd1edfe630c8d0be6e6aab574094c0a2fb8a5d9b
Oscarqjh <aerialguard@gmail.com>
2023-09-25 23:07:11 +0800

    updated user schema

--- VIFitnessBackend/model/model.js
+++ VIFitnessBackend/model/model.js
@@ -6,9 +6,16 @@ const dataSchema = new mongoose.Schema({
     type: String,
   },
   password: {
-    required : true,
-    type : String,
-  }
+    required: true,
+    type: String,
+  },
+  age: { required: true, type: Number, default: 0 },
+  weight: { required: true, type: Number, default: 0 },
+  height: { required: true, type: Number, default: 0 },
+  google_data: { type: Object, default: {} },
+  strava_data: { type: Object, default: {} },
+  meals: { type: Array, default: [] },
+  workouts: { type: Array, default: [] },
 });
 
 const User = mongoose.model("User", dataSchema);
