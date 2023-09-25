d014cb49da2bdeb0fdc26f3eeecc36e407318bb8
ngyh6726 <ngyh6726@gmail.com>
2023-09-25 16:14:22 +0800

    Ng Completed Landing Page

--- VIFitnessFrontend/src/routes/landingPage.jsx
+++ VIFitnessFrontend/src/routes/landingPage.jsx
@@ -8,6 +8,17 @@ import { faArrowDown, faBurger, faCalendar, faCapsules, faChartLine, faMessage }
 
 export default function LandingPage() {
 
+  function initializePage() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });}
+
   useEffect(() => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
@@ -20,6 +31,50 @@ export default function LandingPage() {
     });
   }, []);
 
+  function FadeInLeft(props) {
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+      return () => observer.unobserve(domRef.current);
+    }, []);
+    return (
+      <div
+        className={`fade-in-left ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
+  function FadeInRight(props) {
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+      return () => observer.unobserve(domRef.current);
+    }, []);
+    return (
+      <div
+        className={`fade-in-right ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    initializePage();
+  }, []);
+
   return (
     <>
       <div className="landing-page-wrapper">
@@ -27,53 +82,58 @@ export default function LandingPage() {
 
         
         <div className="image-container">
-        <img
+          <img
             src = {BG}
             alt = "LOGO"></img>
+          <div className="flex flex-col items-center ">
+            <div className="absolute top-3/4 w-full h-1/4 bg-gradient-to-b from-transparent to-black z-20"/>
+          </div>
         </div>
-
+        
 
         <div className="-translate-y-1/2 -translate-x-1/2 flex flex-col items-center justify-center top-2/3 left-1/2 z-10 absolute">
-          <h2 className="cssanimation sequence fadeInBottom font-sans font-bold">Welcome to</h2>
-          <h2 className="cssanimation sequence fadeInBottom font-sans font-bold">VIFitness</h2>
-          <a href="#macros" className="white-link cssanimation sequence fadeInBottom flex flex-col items-center bg-blue-500 rounded-3xl p-2 shadow-md">
+          <h2 className="cssanimation sequence fadeInBottom font-sans font-bold justify-self-center">Welcome to</h2>
+          <h2 className="cssanimation sequence fadeInBottom font-sans font-bold pb-20">VIFitness</h2>
+          <a href="#macros" id="browse" className="cssanimation sequence fadeInBottom flex flex-col items-center bg-black rounded-3xl p-3 shadow-md">
             <div className="text">Browse our features</div>
-            <div classname="text"> <FontAwesomeIcon icon={faArrowDown} size="xl"/></div>
+            <div className="text">
+               <FontAwesomeIcon icon={faArrowDown}/>
+            </div>
           </a>
         </div>
 
-        <div style={{ backgroundColor: '#000000', width: '100%', height: '300px' }}/>
-
-
-
-
-
+ 
         <div className="content-container" id="macros">
-          <div className="content-text">
+          <FadeInLeft>
+            <div className="content-text">
             <div className="content-header">
               <FontAwesomeIcon icon={faBurger} size="xl"/> Track Your Macros
             </div>
             <div className="content-info">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             </div>
-          </div>
+            </div>
+          </FadeInLeft>
           <div className="content-image"/>
         </div>
 
         <div className="content-container">
           <div className="content-image"/>
-          <div className="content-text">
-            <div className="content-header">
-              <FontAwesomeIcon icon={faCalendar} size="xl"/> Personalized Workout Plan
-            </div>
-            <div className="content-info">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            </div>
-          </div>
+          <FadeInRight>
+            <div className="content-text">
+              <div className="content-header">
+                <FontAwesomeIcon icon={faCalendar} size="xl"/> Personalized Workout Plan
+              </div>
+              <div className="content-info">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              </div>
+            </div>  
+          </FadeInRight>
         </div>
 
         <div className="content-container">
-          <div className="content-text">
+          <FadeInLeft>
+            <div className="content-text">
             <div className="content-header">
               <FontAwesomeIcon icon={faMessage} size="xl"/> Community
             </div>
@@ -81,12 +141,14 @@ export default function LandingPage() {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             </div>
           </div>
+          </FadeInLeft>
           <div className="content-image"/>
         </div>
 
         <div className="content-container">
           <div className="content-image"/>
-          <div className="content-text">
+          <FadeInRight>
+            <div className="content-text">
             <div className="content-header">
               <FontAwesomeIcon icon={faCapsules} size="xl"/> Supplementary Recommendations
             </div>
@@ -94,10 +156,12 @@ export default function LandingPage() {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             </div>
           </div>
+          </FadeInRight>
         </div>
 
         <div className="content-container">
-          <div className="content-text">
+          <FadeInLeft>
+            <div className="content-text">
             <div className="content-header">
               <FontAwesomeIcon icon={faChartLine} size="xl"/> Progression Tracking
             </div>
@@ -105,6 +169,7 @@ export default function LandingPage() {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             </div>
           </div>
+          </FadeInLeft>
           <div className="content-image"/>
         </div>
 
