32288c6f6824f3711ffe3b7bbc7bd6e3677dd7cd
bron322 <lebron13145@gmail.com>
2023-09-25 21:54:13 +0800

    Edited a bit

--- VIFitnessFrontend/src/main.jsx
+++ VIFitnessFrontend/src/main.jsx
@@ -40,6 +40,7 @@ import PublicLayout from "./layouts/PublicLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import TestRoute1 from "./routes/TestRoute1";
 import GenerateWorkout from "./routes/GetWorkoutPage";
+import LebronPage from "./routes/LebronPage";
 import StravaRedirect, {
   loader as stravaLoader,
 } from "./routes/StravaRedirect";
