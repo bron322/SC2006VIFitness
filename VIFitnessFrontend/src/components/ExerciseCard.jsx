7b365a53a6c4c047c367db0f5b2f08c1e4939bb0
Phuvj <128466592+Phuvj@users.noreply.github.com>
2023-11-02 00:51:17 +0800

    date picker done

--- VIFitnessFrontend/src/components/ExerciseCard.jsx
+++ VIFitnessFrontend/src/components/ExerciseCard.jsx
@@ -4,25 +4,103 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Calves from "./styles/photos/Calves.png";
+import Glutes from "./styles/photos/Glutes.png";
+import Hamstring from "./styles/photos/Hamstring.jpg";
+import Quads from "./styles/photos/Quads.png";
+import AddWorkoutButton from "../routes/Calendar/components/AddWorkoutButton";
+import SmallCalendar from "../routes/Calendar/components/SmallCalendar";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 3,
+  px: 3,
+  pb: 3,
+};
+
+function ChildModal2() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpen}>Add to calendar</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 400, height: 150 }}>
+          <div className="w-52">
+            <SmallCalendar />
+          </div>
+          <AddWorkoutButton />
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
 
 export default function ExerciseCard({img, title, description}) {
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-      <Card sx={{ width: 150, height: 200 }}>
+    <div>
+      <Card sx={{ width: 150, height: 200 }} onClick={handleOpen}>
         <CardActionArea>
           <CardMedia sx={{ maxWidth: 150, height: 120 }}
             component="img"
             image={img}
           />
           <CardContent>
-            <Typography gutterBottom variant="body1" component="div" className='text-center'>
+            <Typography gutterBottom variant="h3" component="div" className='text-center'>
               {title}
             </Typography>
-            <Typography variant="body1" color="text.secondary" className='text-center'>
+            <Typography variant="h5" color="text.secondary" className='text-center'>
               {description}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 1000, height: 800 }}>
+          <Typography variant="h1" className="text-center">
+            {/* Name of exercise */}
+          </Typography>
+          {/* <h2 id="parent-modal-title">Exercises</h2> */}
+          <div className="flex-grow pb-8">
+            {/* Exercise details */}
+          </div> 
+          <ChildModal2 />
+        </Box>
+      </Modal>
+    </div>
   );
 }
-
