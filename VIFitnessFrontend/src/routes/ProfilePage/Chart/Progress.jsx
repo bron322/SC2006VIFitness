2345235ce07e76c39c7f3e53a709f1b48fc22208
wytan12 <tanweiyin12@gmail.com>
2023-11-04 11:39:37 +0800

    Update dashboard

--- VIFitnessFrontend/src/routes/ProfilePage/Chart/Progress.jsx
+++ VIFitnessFrontend/src/routes/ProfilePage/Chart/Progress.jsx
@@ -8,9 +8,9 @@ function ProgressCircle ({ progress = "0.75", size = "40" }) {
   return (
     <Box
       sx={{
-        background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
-            conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
-            ${colors.greenAccent[500]}`,
+        background: `radial-gradient(${colors.primary.default} 55%, transparent 56%),
+            conic-gradient(transparent 0deg ${angle}deg, ${colors.secondary.default} ${angle}deg 360deg),
+            ${colors.secondary.default}`,
         borderRadius: "50%",
         width: `${size}px`,
         height: `${size}px`,
--- VIFitnessFrontend/src/routes/ProfilePage/pie.jsx
+++ VIFitnessFrontend/src/routes/ProfilePage/pie.jsx
@@ -1,14 +1,14 @@
-import { Box } from "@mui/material";
-import Header from "./Chart/Header";
-import PieChart from "./Chart/Pie";
+// import { Box } from "@mui/material";
+// import Header from "./Chart/Header";
+// import PieChart from "./Chart/Pie";
 
-export default function Pie () {
-  return (
-    <Box m="20px">
-      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
-      <Box height="75vh">
-        <PieChart />
-      </Box>
-    </Box>
-  );
-}
+// export default function Pie () {
+//   return (
+//     <Box m="20px">
+//       <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+//       <Box height="75vh">
+//         <PieChart />
+//       </Box>
+//     </Box>
+//   );
+// }
