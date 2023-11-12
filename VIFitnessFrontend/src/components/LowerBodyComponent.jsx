219ddd2a0591b1f9e6442f037df8cb7ae631356a
bron322 <lebron13145@gmail.com>
2023-11-12 20:25:41 +0800

    hover effects on exercise page

--- VIFitnessFrontend/src/components/LowerBodyComponent.jsx
+++ VIFitnessFrontend/src/components/LowerBodyComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lower from "./styles/photos/LowerBody.jpg";
 import Calves from "./styles/photos/Calves.png";
 import Glutes from "./styles/photos/Glutes.png";
@@ -10,75 +10,82 @@ import MuscleCard from './MuscleCard';
 import { Link } from 'react-router-dom';
 
 function LowerBodyComponent() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <>
-        <div className="flex">
-            <div className="sticky top-0 flex-none w-1/3 h-screen">
-                <Link to="/user/workout-planner">
-                    <img src={Lower} className="h-screen w-full object-cover"
-                    alt="Workout Planner"/>
-                </Link>
-            </div>
-            <div className="flex-grow px-24 pt-10 pb-10">
-                <div className="grid grid-cols-2 grid-rows-3 gap-x-24 gap-y-20 overflow-y-auto">
-
-                    {/* Card 1 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Abductors}
-                        title="Abductors"
-                        description=""
+            <div className="flex">
+                <div className={`sticky sticky-container top-0 flex-none w-1/3 h-screen ${isHovered ? 'hover-return' : ''}`}
+                    onMouseOver={() => setIsHovered(true)}
+                    onMouseOut={() => setIsHovered(false)}
+                >
+                    <Link to="/user/workout-planner">
+                        <img src={Lower} className="h-full w-full object-cover"
+                            alt="Workout Planner"
                         />
-                    </div>
+                         {isHovered && <p className="text-white text-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Return</p>}
+                    </Link>
+                </div>
+                <div className="flex-grow px-24 pt-10 pb-10">
+                    <div className="grid grid-cols-2 grid-rows-3 gap-x-24 gap-y-20 overflow-y-auto">
 
-                    {/* Card 2 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Adductors}
-                        title="Adductors"
-                        description=""
-                        />
-                    </div>
+                        {/* Card 1 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Abductors}
+                                title="Abductors"
+                                description=""
+                            />
+                        </div>
 
-                    {/* Card 3 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Calves}
-                        title="Calves"
-                        description=""
-                        />
-                    </div>
+                        {/* Card 2 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Adductors}
+                                title="Adductors"
+                                description=""
+                            />
+                        </div>
 
-                    {/* Card 4 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Glutes}
-                        title="Glutes"
-                        description=""
-                        />
-                    </div>
+                        {/* Card 3 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Calves}
+                                title="Calves"
+                                description=""
+                            />
+                        </div>
 
-                    {/* Card 5 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Hamstring}
-                        title="Hamstrings"
-                        description=""
-                        />
-                    </div>
+                        {/* Card 4 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Glutes}
+                                title="Glutes"
+                                description=""
+                            />
+                        </div>
 
-                    {/* Card 6 */}
-                    <div className="flex justify-center">
-                        <MuscleCard
-                        img={Quads}
-                        title="Quadriceps"
-                        description=""
-                        />
-                    </div>
+                        {/* Card 5 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Hamstring}
+                                title="Hamstrings"
+                                description=""
+                            />
+                        </div>
 
+                        {/* Card 6 */}
+                        <div className="flex justify-center">
+                            <MuscleCard
+                                img={Quads}
+                                title="Quadriceps"
+                                description=""
+                            />
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
--- VIFitnessFrontend/src/components/PlannerComponent.jsx
+++ VIFitnessFrontend/src/components/PlannerComponent.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "../components/styles/WorkoutBackground.css"; 
+import "../components/styles/WorkoutBackground.css";
 import Upper from "./styles/photos/UpperBody.jpg";
 import Lower from "./styles/photos/LowerBody.jpg";
 import Core from "./styles/photos/Core.jpg";
@@ -8,41 +8,45 @@ import LowerButton from './LowerButton';
 import UpperButton from './UpperButton';
 import CoreButton from './CoreButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
 const BackgroundImages = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
   const images = [
-    Lower, 
+    Lower,
     Upper,
     Core,
   ];
 
-const texts = ['Lower Body', 'Upper Body', 'Core'];
-const paths = ['/user/workout-lower','/user/workout-upper','/user/workout-core']
-const part1 = ['Quads','Chest','Abs'];
-const part2 = ['Hamstrings','Biceps','Obliques']
-const part3 = ['Glutes','Shoulders']
-const part4 = ['Calves','Lats']
-const buttons = [<LowerButton/>,<UpperButton/>,<CoreButton/>]
+  const texts = ['Lower Body', 'Upper Body', 'Core'];
+  const paths = ['/user/workout-lower', '/user/workout-upper', '/user/workout-core']
+  const part1 = ['Quads', 'Chest', 'Abs'];
+  const part2 = ['Hamstrings', 'Biceps', 'Obliques']
+  const part3 = ['Glutes', 'Shoulders']
+  const part4 = ['Calves', 'Lats']
+  const buttons = [<LowerButton />, <UpperButton />, <CoreButton />]
 
   return (
     <div className="flex h-screen">
       {images.map((image, index) => (
         <div
           key={index}
-          className="background-image"
-          style={{ backgroundImage: `url(${image})` }}>
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
+          className={`background-image ${hoveredIndex === index ? 'hover-effect' : ''}`}
+          style={{ backgroundImage: `url(${image})` }}
+          onMouseOver={() => setHoveredIndex(index)}
+          onMouseOut={() => setHoveredIndex(null)}
+        >
+          <div className={`absolute inset-0 bg-gradient-to-b from-transparent to-black ${hoveredIndex === index ? 'hover-effect' : ''}`}></div>
           <div className="flex flex-col justify-center items-center">
-            <Link to= {paths[index]} className="z-10 text-4xl bg-transparent h-1/2">
-                {texts[index]}
+            <Link to={paths[index]} className={`z-10 text-4xl bg-transparent h-1/2 ${hoveredIndex === index ? 'hovered-link' : ''}`}>
+              {texts[index]}
             </Link>
             <div className="z-10 text-center items-center">{buttons[index]} </div>
-            <div className="z-10 text-xl text-center bg-transparent">{part1[index]}</div>  
-            <div className="z-10 text-xl text-center bg-transparent">{part2[index]}</div>  
-            <div className="z-10 text-xl text-center bg-transparent">{part3[index]}</div> 
-            <div className="z-10 text-xl text-center bg-transparent">{part4[index]}</div>   
+            <div className="z-10 text-xl text-center bg-transparent">{part1[index]}</div>
+            <div className="z-10 text-xl text-center bg-transparent">{part2[index]}</div>
+            <div className="z-10 text-xl text-center bg-transparent">{part3[index]}</div>
+            <div className="z-10 text-xl text-center bg-transparent">{part4[index]}</div>
           </div>
         </div>
       ))}
