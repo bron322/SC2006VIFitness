c333993f51a8d5b1212d8c8a893cb9476eee0f38
ngyh6726 <ngyh6726@gmail.com>
2023-11-12 23:13:05 +0800

    Revert "Merge pull request #168 from bron322/NgLandingPageBranch"
    This reverts commit 47e658e636e12e6334b5b21fe46f6d93fd5ef6f1, reversing
    changes made to 5dd41e844637de6a68d425c2f7ed1ee7c88da91a.

--- VIFitnessFrontend/src/components/Experience.jsx
+++ VIFitnessFrontend/src/components/Experience.jsx
@@ -1,28 +1,14 @@
 import { OrbitControls } from "@react-three/drei";
-import Woman from "./Woman";
+import React from "react";
 
 const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <ambientLight />
-      <directionalLight
-        position={[-5, 5, 5]}
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-      />
-      <group position={[0, -1, 0]}>
-        <Woman />
-      </group>
-      <mesh
-        rotation={[-0.5 * Math.PI, 0, 0]}
-        position={[0, -1, 0]}
-        receiveShadow
-      >
-        <planeGeometry args={[10, 10, 1, 1]} />
-        <shadowMaterial transparent opacity={0.2} />
-      </mesh>
+      <OrbitControls/>
+        <mesh>
+          <boxGeometry />
+          <meshNormalMaterial />
+        </mesh>
     </>
   );
 };
