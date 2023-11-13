192b8787cf64947f7d5ad434d920b7aabdaea7d6
ngyh6726 <ngyh6726@gmail.com>
2023-11-13 14:55:41 +0800

    Revert "Merge pull request #173 from bron322/ng"
    This reverts commit 0403d32c6eee9c30cc7d3b1959714ad5132b418c, reversing
    changes made to 7cc9eda16f7daf6f8baf96c594e30833b1896911.

--- VIFitnessFrontend/src/routes/Calendar/components/Sidebar.jsx
+++ VIFitnessFrontend/src/routes/Calendar/components/Sidebar.jsx
@@ -2,12 +2,42 @@ import React from "react";
 import CreateEventButton from "./CreateEventButton";
 import SmallCalendar from "./SmallCalendar";
 import Labels from "./Labels";
-export default function Sidebar() {
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
+
+export default function Sidebar({ setSelectedMuscleGroup }) {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <aside className="border p-5 w-64">
       {/* <CreateEventButton /> */}
       <SmallCalendar />
       {/* <Labels /> */}
+      <div className="mb-2 mt-16 w-56">
+        <label htmlFor="muscle" className="w-full text-xl">Select Muscle Group:</label>
+        <select
+          id="muscle"
+          onChange={(e) => setSelectedMuscleGroup(e.target.value)}
+          style={{ backgroundColor: colors.accent.default, width: "80%" }}
+        >
+          <option value="">All</option>
+          <option value="abdominals">Abdominals</option>
+          <option value="abductors">Abductors</option>
+          <option value="adductors">Adductors</option>
+          <option value="biceps">Biceps</option>
+          <option value="calves">Calves</option>
+          <option value="chest">Chest</option>
+          <option value="forearms">Forearms</option>
+          <option value="glutes">Glutes</option>
+          <option value="hamstrings">Hamstrings</option>
+          <option value="lats">Lats</option>
+          <option value="lower_back">Lower Back</option>
+          <option value="middle_back">Middle Back</option>
+          <option value="quadriceps">Quadriceps</option>
+          <option value="triceps">Triceps</option>
+        </select>
+      </div>
     </aside>
   );
 }
