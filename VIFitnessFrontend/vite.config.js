4c03b54e969a3943edc478bcc1384b1c8623364e
oscarqjh <oscar.jh9@gmail.com>
2023-11-13 16:02:33 +0800

    test for production

--- VIFitnessFrontend/vite.config.js
+++ VIFitnessFrontend/vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      external: "@mantine/core",
+    },
+  },
 });
